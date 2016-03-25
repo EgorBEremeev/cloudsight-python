624dd7bf9e4b021550f488bcd55561a5bfd05146
Kamil Klimkiewicz <miGlanz@gmail.com>
2016-03-25 11:51:36 +0100

    Update package information

--- setup.py
+++ setup.py
@@ -1,10 +1,19 @@
 from setuptools import find_packages, setup
 
 
+LONG_DESCRIPTION = """\
+Cloudsight API for Python
+=========================
+
+A simple CloudSight API Client for Python programming language. It has been
+tested with Python versions 2.7 and 3.5."""
+
+
 setup(
     name='cloudsight',
-    version='0.8',
+    version='0.9',
     description='Cloudsight API library for Python',
+    long_description=LONG_DESCRIPTION,
     license='MIT',
     author='Cloudsight',
     author_email='miGlanz@gmail.com',
